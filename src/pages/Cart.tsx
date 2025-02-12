961650853a3bc1e0297be6719fea4210bd1a9dac
Gabrielloop <gabriel.henin@gmail.com>
2025-02-12 17:51:17 +0100

    Add cart total calculation and order handling in Cart component; create ValidateCommand component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -11,15 +11,51 @@ export const cartObservable: BehaviorSubject<CartProps[]> = new BehaviorSubject<
 const Cart: React.FC = () => {
   const [productsInCart, setProductsInCart] = useState<CartProps[]>([]);
   const [cartSize, setCartSize] = useState<number>(0);
+  const [cartTotal, setCartTotal] = useState<number>(0);
+  const [mail, setMail] = useState<string>("");
 
   // récupération du panier
   useEffect(() => {
     const subscription = cartObservable.subscribe((newCart) => {
       setProductsInCart(newCart);
       setCartSize(newCart.reduce((acc, item) => acc + item.quantity, 0));
+      setCartTotal(
+        newCart.reduce(
+          (acc, item) => acc + item.quantity * item.product.productPrice,
+          0
+        )
+      );
     });
     return () => subscription.unsubscribe();
   }, []);
+
+  const handleMail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMail(e.target.value);
+  };
+
+  const handleOrder = () => {
+    const products = productsInCart.map((product) => ({
+      productId: product.product.productId,
+      quantity: product.quantity,
+      total: product.quantity * product.product.productPrice,
+    }));
+    const totalCommand = products.reduce((acc, item) => acc + item.total, 0);
+
+    console.log(
+      "Commande envoyée",
+      products,
+      " mail : ",
+      mail,
+      "total",
+      totalCommand
+    );
+
+    // POST vers l'api pour enregistrer la commande
+
+    // si return true : vider le panier
+    console.log("Commande enregistrée : panier vidé");
+    cartObservable.next([]);
+  };
 
   return (
     <div>
@@ -32,6 +68,10 @@ const Cart: React.FC = () => {
           <CartList key={product.product.productId} {...product} />
         ))}
       </ul>
+      <h3>Total: {cartTotal} €</h3>
+
+      <input onChange={handleMail} type="text" placeholder="votre mail" />
+      <button onClick={handleOrder}>commander</button>
     </div>
   );
 };
